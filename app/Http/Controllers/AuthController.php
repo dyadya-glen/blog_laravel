4feffee7748182dd81740f78a59f5bda48e36433
Roman Gulyaev <glen2001@yandex.ru>
2019-02-06 23:45:55 +0300

    Added validation of the registration form

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,33 +20,40 @@ class AuthController extends Controller
 
     public function postingSignInData(Request $request)
     {
-        $errors = [];
-        $email = $request->input('email');
-        $password = $request->input('password');
-
-        if ($request->filled($email)) {
-            $errors[] = 'Поле Email обязательно для заполнения!';
-        }
-        elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $errors[] = 'Поле "адрес e-mail" должно быть действительным электронным адресом.';
-        }
-
-        if ($request->filled($password)) {
-            $errors[] = 'Поле Пароль обязательно для заполнения!';
-        }
-        elseif (mb_strlen($password) < 5) {
-            $errors[] = 'Поле Пароль должно быть не менее пяти символов!';
-        }
-
-        if (count($errors) === 0) {
-            return redirect()->route('homePage');
-        }
+        $this->validate($request, [
+            'name' => 'required|min:2|alpha_num',
+            'email' => 'required|email',
+            'password' => 'required|min:6|max:32',
+            'password_confirm' => 'required|same:password',
+            'is_confirmed' => 'accepted',
+        ]);
+//        $errors = [];
+//        $email = $request->input('email');
+//        $password = $request->input('password');
+//
+//        if ($request->filled($email)) {
+//            $errors[] = 'Поле Email обязательно для заполнения!';
+//        }
+//        elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+//            $errors[] = 'Поле "адрес e-mail" должно быть действительным электронным адресом.';
+//        }
+//
+//        if ($request->filled($password)) {
+//            $errors[] = 'Поле Пароль обязательно для заполнения!';
+//        }
+//        elseif (mb_strlen($password) < 5) {
+//            $errors[] = 'Поле Пароль должно быть не менее пяти символов!';
+//        }
+//
+//        if (count($errors) === 0) {
+//            return redirect()->route('homePage');
+//        }
 
         return view('layouts.secondary',[
             'page' => 'pages.sign-in',
             'title' => 'Вход',
-            'errors' => $errors,
-            'email' => $email,
+//            'errors' => $errors,
+//            'email' => $email,
         ]);
     }
 
@@ -58,49 +65,20 @@ class AuthController extends Controller
     {
         return view('layouts.secondary',[
             'page' => 'pages.sign-up',
-            'title' => 'Регистрация'
+            'title' => 'Регистрация',
         ]);
     }
 
     public function postingSignUpData(Request $request)
     {
-        $errors = [];
-        $email = $request->input('email');
-        $password = $request->input('password');
-        $passwordConfirm = $request->input('password_confirm');
-        $name = $request->input('name');
-        $phone = $request->input('phone');
-
-        if ($request->filled($email)) {
-            $errors[] = 'Поле Email обязательно для заполнения!';
-        }
-        elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $errors[] = 'Поле "адрес e-mail" должно быть действительным электронным адресом.';
-        }
-
-        if ($request->filled($password)) {
-            $errors[] = 'Поле Пароль обязательно для заполнения!';
-        }
-        elseif (mb_strlen($password) < 5) {
-            $errors[] = 'Поле Пароль должно быть не менее пяти символов!';
-        }
-
-        if ($request->filled($password)) {
-            $errors[] = 'Поле Подтверждение обязательно для заполнения!';
-        }
-        elseif ($passwordConfirm !== $password) {
-            $errors[] = 'Поля Пароль и Подтверждение не совпадают';
-        }
-
-        return view('layouts.secondary',[
-            'page' => 'pages.sign-up',
-            'title' => 'Регистрация',
-            'errors' => $errors,
-            'email' => $email,
-//            'password' => $password,
-//            'passwordConfirm' => $passwordConfirm,
-            'name' => $name,
-            'phone' => $phone
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required|min:6|max:32',
+            'password_confirm' => 'required|same:password',
+            'is_confirmed' => 'accepted',
+            'name' => 'required|min:2|alpha_num',
+            'phone' => 'nullable|regex:/^(\+?\d{1,3}\s?\(\d{3}\)\s?\d{3}(-\d{2}){2})?$/'
         ]);
+        return back();
     }
 }
